ab2dfaec32eed900b4c5ccdcdefa03bf341e80bc
elenabeznossenko <elena.beznossenko@gmail.com>
2022-08-11 22:32:02 +0500

    exercise4

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int number;
+int max = int.MinValue;
+for(int i = 0; i < 3; i++);
+number = Convert.ToInt32(Console.ReadLine());
+if (max < number)
+{
+    max = number;
+}
+Console.WriteLine(max);
